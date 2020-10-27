37798b8d4c493a690a42ece478d9801522a605db
Shafayeat Kabir <shafayeatkabir@gmail.com>
2020-10-27 12:03:44 +0600

    smooth vibration implemented for android

---
+++ android/app/src/main/java/com/three_thoughts/VibrationModule.java
@@ -0,0 +1,45 @@
+// VibrationModule.java
+
+package com.three_thoughts;
+
+import android.accessibilityservice.AccessibilityService;
+import android.widget.Toast;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+import java.util.HashMap;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.os.Build;
+import android.content.Context;
+
+
+public class VibrationModule extends ReactContextBaseJavaModule {
+  private static ReactApplicationContext reactContext;
+
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  VibrationModule(ReactApplicationContext context) {
+    super(context);
+    reactContext = context;
+  }
+  @Override
+  public String getName() {
+    return "AndroidVibration";
+  }  
+  @ReactMethod
+  public void show( int duration, int amplitude) {
+      Vibrator v = (Vibrator) reactContext.getSystemService(Context.VIBRATOR_SERVICE);
+      if (Build.VERSION.SDK_INT >= 26 ) {
+          v.vibrate(VibrationEffect.createOneShot(duration, amplitude));
+      } else {
+          v.vibrate(duration);
+      }    
+  }  
+}
--- src/screens/content/Content.js
+++ src/screens/content/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Dimensions,
@@ -6,142 +6,71 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  NativeModules,
   Platform,
+  Slider,
   StyleSheet,
   Alert,
 } from 'react-native';
 // import styles from './Content.styles';
 import {Colors, FontType} from '../../helpers/theme';
 import WaveView from '../../components/WaveView';
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  // Text,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
-import {ScreenWidth} from '../../helpers/constants/common';
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
-const CircleCircumference = 2 * Math.PI * 160;
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-    this.animatedOffSet = new Animated.Value(CircleCircumference);
-    this.animatedRadius = new Animated.Value(85);
-  }
-
-  animate = () => {
-    Animated.parallel([
-      Animated.timing(this.animatedOffSet, {
-        toValue: 0,
-        duration: 3000,
-        useNativeDriver: true,
-      }),
-      Animated.timing(this.animatedRadius, {
-        toValue: 158,
-        duration: 3000,
-        useNativeDriver: true,
-      }),
-    ]).start();
+const Content = (props) => {
+  const [apmlitude, setAmplitude] = useState(20);
+  const androidVibrate = () => {
+    Platform.OS === 'android' &&
+      NativeModules.AndroidVibration.show(2000, apmlitude);
   };
-
-  componentDidMount() {
-    this.animate();
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.svgHolder}>
-          <View style={styles.textHolder}>
-            <Text style={styles.text}>
-              Building your{'\n'}
-              exercise
-            </Text>
-          </View>
-          <Svg height="100%" width="100%" style={styles.svg}>
-            <G rotation="-90" origin={('160', '160')}>
-              <AnimatedCircle
-                cx="158"
-                cy="162"
-                r="160"
-                stroke="#447d70"
-                strokeWidth="2"
-                fill="none"
-                strokeDasharray={CircleCircumference}
-                strokeDashoffset={this.animatedOffSet}
-              />
-              <AnimatedCircle
-                cx="158"
-                cy="162"
-                r={this.animatedRadius}
-                strokeWidth="0"
-                fill={Colors.cornflowerBlue}
-                strokeDasharray={CircleCircumference}
-                strokeDashoffset={this.animatedOffSet}
-              />
-              <Circle
-                cx="158"
-                cy="162"
-                r="85"
-                strokeWidth="0"
-                fill={'#13172f'}
-                strokeDasharray={CircleCircumference}
-                strokeDashoffset={this.animatedOffSet}
-              />
-            </G>
-          </Svg>
-        </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Slider
+          value={apmlitude}
+          style={{
+            height: 50,
+            width: 220,
+            alignSelf: 'center',
+          }}
+          minimumValue={5}
+          maximumValue={255}
+          step={5}
+          onValueChange={(value) => setAmplitude(value)}
+        />
+        <Text style={styles.txt}>Intensity: {apmlitude}</Text>
       </View>
-    );
-  }
-}
+      <TouchableOpacity style={styles.btn} onPress={androidVibrate}>
+        <Text style={styles.txt}>Click</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default Content;
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFill,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  svgHolder: {
-    height: 324,
-    width: 324,
-    marginBottom: 80,
-    alignItems: 'center',
-    // backgroundColor: 'red',
-  },
-  textHolder: {
-    height: 170,
-    width: 170,
-    borderRadius: 85,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute',
-    zIndex: 33,
-    bottom: 75,
-    // backgroundColor: 'red',
   },
-  text: {
+  btn: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    backgroundColor: 'tomato',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txt: {
+    fontFamily: FontType.Medium,
     color: 'white',
-    fontSize: 16,
-    textAlign: 'center',
-    fontFamily: FontType.SemiBold,
+    fontSize: 22,
+  },
+  topContainer: {
+    height: 300,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'yellow',
   },
 });
